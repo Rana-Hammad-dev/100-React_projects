c7f0c4ad1f2d3515a9509442abc21026d3068e23
Rana Hammad Ali <ranahammadaliseo@gmail.com>
2025-11-04 19:06:35 +0500

    Adding project-1 Conditional Rendering

--- 01-Conditional-Rendering/1.Login-Toggle/src/App.jsx
+++ 01-Conditional-Rendering/1.Login-Toggle/src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
-    <div>
-      app
+    <div className='login'>
+      <h1>{isLogin ? "Please login" : "Welcome!..."}</h1>
+      <button className={isLogin ? "green" : "red"} onClick={() => setIsLogin(!isLogin)}>{isLogin ? "Login in" : "Log out"}</button>
     </div>
   )
 }
